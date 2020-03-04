b4415f9d8d47ce2dc014643d9ea1a33e21b77d35
Raiana Sousa dos Santos <53270687+raianasousa@users.noreply.github.com>
2020-03-04 20:36:47 -0300

    Add files via upload
    Apresentação de acervo de livros inseridos.

---
+++ Acervo.java
@@ -0,0 +1,11 @@
+class Acervo{
+ String titulo;
+ String editora;
+ String autor;
+void Livro(){
+    System.out.println(titulo);
+    System.out.println(editora);
+    System.out.println(autor);
+  }
+}
+
---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+  public static void main(String[] args){
+   Acervo livro = new Acervo();
+
+   livro.titulo = "Harry Potter";
+   livro.editora = "Pottermore Limited";
+   livro.autor = "JK Rowling";
+
+   livro.Livro();
+  }
+}
